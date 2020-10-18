d5e5ac2dc9606d762e913454f3ab4826feae4762
WhiskeeDev <email@liamholmes.co.uk>
2020-10-18 03:38:11 +0100

    on bot boot, check guild for members that should have the warns role

--- modules/Moderation/index.js
+++ modules/Moderation/index.js
@@ -111,10 +111,28 @@ client.on("message", message => {
 
 client.on("guildMemberAdd", member => {
     writeLog(`${member.user.tag} Joined the server!`.rainbow)
+    if (!config.warnsRoleID) return false
     const usersWarnProfile = warnedUsers[member.user.id]
 
     if (usersWarnProfile && usersWarnProfile.warns.length) {
-        if (config.warnsRoleID) member.roles.add(config.warnsRoleID)
-        writeLog(`Gave ${member.user.tag} the warns role because they already have warns`.red)
+        member.roles.add(config.warnsRoleID)
+        writeLog(`Gave ${member.user.tag} the 'warns' role because they already have warns`.red)
+    }
+})
+
+client.on("ready", async () => {
+    if (!config.warnsRoleID) return false
+    const usersToCheck = Object.keys(warnedUsers)
+    const guild = await client.guilds.fetch(process.env.guildID)
+
+    if (guild) {
+        const members = guild.members.cache
+        usersToCheck.forEach(u => {
+            const guildMember = members.get(u)
+            if (guildMember) {
+                guildMember.roles.add(config.warnsRoleID)
+                writeLog(`Gave ${guildMember.user.tag} the 'warns' role because they already have warns`.red)
+            }
+        })
     }
 })
