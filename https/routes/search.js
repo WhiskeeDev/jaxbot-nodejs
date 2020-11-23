4d455b97d25047812e0375eb68aa716f584c1926
WhiskeeDev <email@liamholmes.co.uk>
2020-11-23 11:20:14 +0000

    ok maybe we should test some things...

--- https/routes/search.js
+++ https/routes/search.js
@@ -25,7 +25,7 @@ module.exports = {
                 }
               ]
             })
-            const allBans = await process.database.models.Warn.findAll({
+            const allBans = await process.database.models.Ban.findAll({
               order: [['createdAt', 'DESC']],
               include: [
                 process.database.models.User,
