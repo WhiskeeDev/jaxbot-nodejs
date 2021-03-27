72fa0e4abdf4e66936c7e200c5aeb1189c9c0309
WhiskeeDev <email@liamholmes.co.uk>
2021-03-27 16:04:28 +0000

    actually commit the damn new file

---
+++ queue.js
@@ -0,0 +1,63 @@
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors')
+const { Op } = require('sequelize')
+const { Trash } = process.database.models
+const client = process.discordClient
+
+const titleCard = '[Queue]'
+const delay = 10
+
+console.log(`${titleCard} Starting queue, I will run ever ${delay} seconds checking for _trash_.`.cyan)
+
+async function processQueue () {
+  console.log(`${titleCard} Checking trash for any due/overdue collections...`.cyan)
+
+  Trash.findAll({
+    where: {
+      collection_time: {
+        [Op.lte]: new Date()
+      }
+    }
+  }).then(async trashes => {
+    if (!trashes || !trashes.length) {
+      setTimeout(() => processQueue(), delay * 1000)
+      return
+    }
+
+    console.log(`${titleCard} Found ${trashes.length} due collection(s)`)
+
+    await Promise.all(trashes.map(async trash => {
+      await processTrash(trash)
+    }))
+
+    setTimeout(() => processQueue(), delay * 1000)
+  })
+}
+
+processQueue()
+
+async function processTrash(trash) {
+  console.log(`${titleCard} Processing Trash [${trash.id}] - ${trash.trash_type}`.cyan)
+  switch (trash.trash_type) {
+  case 'message':
+    await client.channels.cache.each(async channel => {
+      if (channel.type !== 'text') return
+      await channel.messages.fetch(trash.item_id).then(async message => {
+        if (!message) return
+        await message.delete()
+          .then(() => {
+            trash.destroy()
+            console.log(`${titleCard} Successfully removed message`.green)
+          })
+          .catch(displayError(trash))
+      }).catch(() => {})
+    })
+    break
+  default:
+    console.log(`${titleCard} The trash type of "${trash.trash_type}" is unknown, and can not be processed.`.red)
+  }
+}
+
+function displayError ({ id, trash_type, item_id }) {
+  console.log(`${titleCard} Something went wrong when trying to collect (${trash_type}) ${item_id} [Trash-${id}], will try again in next batch.`.red)
+}
